ad398391c31788b9fcff781cc0441623686c0da3
Joseph Woodland <joseph@workit-app.com>
2021-07-02 19:19:27 +0100

    CSS styling to the Modal : Position and the background tint

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,8 +1,20 @@
-import * as model from './model.js';
+// Import data and functions 
+
+import * as model from "./model.js";
+
+// Element Selectors //
+
+// Input Selectors //
+
+// COnatanier Selcetors //
+
+// Button Selectors //
+
+
 
 // Page Load Intital Functions //
-function init (){
-    model.starterMessage();
+function init() {
+  model.starterMessage();
 }
 
 // model.userData();
