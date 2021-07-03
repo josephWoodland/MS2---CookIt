a6d798388af0277e76e6f43bc9ccfb4b0553aee9
Joseph Woodland <joseph@workit-app.com>
2021-07-03 09:34:30 +0100

    Close icon added

--- assets/js/views/input.js
+++ assets/js/views/input.js
@@ -13,7 +13,8 @@ export const openModal = function () {
 
 // Function to close the modal
 export const closeModal = function () {
-  // Click outside the modal - 
+  // Click outside the modal - this took me a while as the first function would close if I clicked anywhere 
+  // on the screen including the form - changed the formModal html class to overlay and did not wrap the form inside the div.
   modal.classList.add("hidden");
   form.classList.add("hidden");
   // Click X button
