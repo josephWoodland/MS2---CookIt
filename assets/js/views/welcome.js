ba3f68d79ed7bc9a1e6dd475efe85e4d3f848c83
Joseph Woodland <joseph@workit-app.com>
2021-07-03 18:18:57 +0100

    Personalised message for the input form

--- assets/js/views/welcome.js
+++ assets/js/views/welcome.js
@@ -1,17 +1,23 @@
 const welcomeMessage = document.querySelector(".main__center-welcomeMessage");
-const formTitle = document.querySelector('.form__header');
+const formTitle = document.querySelector(".form__header");
+const formP = document.querySelector(".form__paragraph");
+const formName = document.querySelector("#formName");
+let name;
 
-
-
+// Runction to render users name to the welcom screen
 export function renderName() {
   // Use the name sored in local storage and print it to the welcome page HTML
-  const name = localStorage.getItem("name");
+  name = localStorage.getItem("name");
   welcomeMessage.textContent = `Welcome Back ${name}, Lets Get Started`;
 }
 
-export function hideName(){
-  // Change class for header
-
-  // Change p
+// Function to render
+export function hideName() {
+  console.log("We have your name on file");
+  // Change the text content for the header
+  formTitle.textContent = `Ok ${name} Let's Create a Meal Plan`;
+  // Change the form paragraph
+  formP.textContent = "";
   // Hide the what is your name question
+  formName.classList.add("hidden");
 }
