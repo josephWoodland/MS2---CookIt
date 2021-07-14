3382548042f922b0c188ac54dae2c677fd21fb7d
Joseph Woodland <joseph@workit-app.com>
2021-07-14 14:46:38 +0100

    Responsive layout of the table for mobile

--- assets/js/views/table.js
+++ assets/js/views/table.js
@@ -96,34 +96,34 @@ export function renderWeekly(plan) {
           </div>
         </div>
         <div class="table__weekly grid-weekly">
-          <div class="table__weekly-cell days">Monday</div>
-          <div class="table__weekly-cell days">Tuesday</div>
-          <div class="table__weekly-cell days">Wednesday</div>
-          <div class="table__weekly-cell days">Thursday</div>
-          <div class="table__weekly-cell days">Friday</div>
-          <div class="table__weekly-cell days">Saturday</div>
-          <div class="table__weekly-cell days">Sunday</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.mon[0].id}">${plan.mon[0].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.tue[0].id}">${plan.tue[0].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.wed[0].id}">${plan.wed[0].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.thur[0].id}">${plan.thur[0].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.fri[0].id}">${plan.fri[0].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.sat[0].id}">${plan.sat[0].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.sun[0].id}">${plan.sun[0].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.mon[1].id}">${plan.mon[1].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.tue[1].id}">${plan.tue[1].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.wed[1].id}">${plan.wed[1].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.thur[1].id}">${plan.thur[1].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.fri[1].id}">${plan.fri[1].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.sat[1].id}">${plan.sat[1].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.sun[1].id}">${plan.sun[1].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.mon[2].id}">${plan.mon[2].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.tue[2].id}">${plan.tue[2].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.wed[2].id}">${plan.wed[2].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.thur[2].id}">${plan.thur[2].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.fri[2].id}">${plan.fri[2].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.sat[2].id}">${plan.sat[2].title}</div>
-          <div class="table__weekly-cell" onClick="getID(this.id)" id="${plan.sun[2].id}">${plan.sun[2].title}</div>
+          <div class="table__weekly-cell days-1">Monday</div>
+          <div class="table__weekly-cell days-2">Tuesday</div>
+          <div class="table__weekly-cell days-3">Wednesday</div>
+          <div class="table__weekly-cell days-4">Thursday</div>
+          <div class="table__weekly-cell days-5">Friday</div>
+          <div class="table__weekly-cell days-6">Saturday</div>
+          <div class="table__weekly-cell days-7">Sunday</div>
+          <div class="table__weekly-cell recipe-1" onClick="getID(this.id)" id="${plan.mon[0].id}">${plan.mon[0].title}</div>
+          <div class="table__weekly-cell recipe-2" onClick="getID(this.id)" id="${plan.tue[0].id}">${plan.tue[0].title}</div>
+          <div class="table__weekly-cell recipe-3" onClick="getID(this.id)" id="${plan.wed[0].id}">${plan.wed[0].title}</div>
+          <div class="table__weekly-cell recipe-4" onClick="getID(this.id)" id="${plan.thur[0].id}">${plan.thur[0].title}</div>
+          <div class="table__weekly-cell recipe-5" onClick="getID(this.id)" id="${plan.fri[0].id}">${plan.fri[0].title}</div>
+          <div class="table__weekly-cell recipe-6" onClick="getID(this.id)" id="${plan.sat[0].id}">${plan.sat[0].title}</div>
+          <div class="table__weekly-cell recipe-7" onClick="getID(this.id)" id="${plan.sun[0].id}">${plan.sun[0].title}</div>
+          <div class="table__weekly-cell recipe-8" onClick="getID(this.id)" id="${plan.mon[1].id}">${plan.mon[1].title}</div>
+          <div class="table__weekly-cell recipe-9" onClick="getID(this.id)" id="${plan.tue[1].id}">${plan.tue[1].title}</div>
+          <div class="table__weekly-cell recipe-10" onClick="getID(this.id)" id="${plan.wed[1].id}">${plan.wed[1].title}</div>
+          <div class="table__weekly-cell recipe-11" onClick="getID(this.id)" id="${plan.thur[1].id}">${plan.thur[1].title}</div>
+          <div class="table__weekly-cell recipe-12" onClick="getID(this.id)" id="${plan.fri[1].id}">${plan.fri[1].title}</div>
+          <div class="table__weekly-cell recipe-13" onClick="getID(this.id)" id="${plan.sat[1].id}">${plan.sat[1].title}</div>
+          <div class="table__weekly-cell recipe-14" onClick="getID(this.id)" id="${plan.sun[1].id}">${plan.sun[1].title}</div>
+          <div class="table__weekly-cell recipe-15" onClick="getID(this.id)" id="${plan.mon[2].id}">${plan.mon[2].title}</div>
+          <div class="table__weekly-cell recipe-16" onClick="getID(this.id)" id="${plan.tue[2].id}">${plan.tue[2].title}</div>
+          <div class="table__weekly-cell recipe-17" onClick="getID(this.id)" id="${plan.wed[2].id}">${plan.wed[2].title}</div>
+          <div class="table__weekly-cell recipe-18" onClick="getID(this.id)" id="${plan.thur[2].id}">${plan.thur[2].title}</div>
+          <div class="table__weekly-cell recipe-19" onClick="getID(this.id)" id="${plan.fri[2].id}">${plan.fri[2].title}</div>
+          <div class="table__weekly-cell recipe-20" onClick="getID(this.id)" id="${plan.sat[2].id}">${plan.sat[2].title}</div>
+          <div class="table__weekly-cell recipe-21" onClick="getID(this.id)" id="${plan.sun[2].id}">${plan.sun[2].title}</div>
         </div>
         <div class="space"></div>
       </div>
