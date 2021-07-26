cc1343305786e4890d770470bca5d20b19d22e7e
Joseph Woodland <joseph@workit-app.com>
2021-07-26 15:51:58 +0100

    Deleted the options js and scss file moved to the settings files

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,15 +1,11 @@
 // Import data and functions
 
 import * as model from "./model.js";
-import * as input from "./views/input.js";
-import * as clearStorage from "./views/clear.js";
 import * as save from "./views/save.js";
-import * as home from "./views/home.js";
 import * as settings from "./views/settings.js";
 import * as recipe from "./views/recipe.js";
 import * as table from "./views/table.js";
 import * as plans from "./views/plans.js";
-import * as options from "./views/options.js";
 import * as form from "./views/form.js";
 
 // Global scope function so it can be used in the HTML
@@ -30,6 +26,7 @@ window.renderSettingsPage = settings.renderSettingsPage;
 window.pageReload = reloadPage;
 window.editSaveName = save.editSaveName;
 window.deleteItem = save.deleteItem;
+window.renderSettingsPage = settings.renderSettings;
 
 /**
  * @param {Event} e
--- assets/js/views/settings.js
+++ assets/js/views/settings.js
@@ -1,51 +1,108 @@
-import * as input from "./input.js";
 import * as model from "../model.js";
-
-const mainContainer = document.querySelector("#mainCenter");
+import * as control from "../controller.js";
 
 /**
  * @param {Event} buttonClick
  *
- * @return {String} render to HTML
+ * @return {String} to print to HTML
  */
+export function selectBackgroundColour() {
+  const items = { ...localStorage };
+  const selection = document.querySelector("#backgroundColourSelect");
+  const html = document.querySelector("html");
+  const userChoice = selection.value;
 
-export function openSettings() {
+  if (userChoice === "default") {
+    if ("background" in items) localStorage.removeItem("background");
+    control.reloadPage();
+    return;
+  } else {
+    html.style.backgroundColor = userChoice;
+    localStorage.setItem("background", userChoice);
+    model.displayOptions("background", userChoice);
+  }
+}
+/**
+ * @param {Event} buttonClick
+ *
+ * @return {String} to print to HTML
+ */
+export function selectTextColour() {
+  const items = { ...localStorage };
+  const selection = document.querySelector("#textColourSelect");
+  const userChoice = selection.value;
+  const html = document.querySelector("html");
+
+  if (userChoice === "default") {
+    if ("text" in items) localStorage.removeItem("text");
+    control.reloadPage();
+    return;
+  } else {
+    html.style.color = userChoice;
+    localStorage.setItem("text", userChoice);
+  }
+}
+/**
+ * @param {Event} buttonClick
+ *
+ * @return {} alters CSS code
+ */
+export function selectBtnColour() {
+  const items = { ...localStorage };
+  const selection = document.querySelector("#btnColourSelect");
+  const userChoice = selection.value;
+  const button = document.querySelectorAll(".btn");
+
+  if (userChoice === "default") {
+    if ("button" in items) localStorage.removeItem("button");
+    control.reloadPage();
+    return;
+  } else {
+    localStorage.setItem("button", userChoice);
+    let i;
+    for (i = 0; i < button.length; i++) {
+      button[i].style.backgroundColor = userChoice;
+    }
+  }
+}
+/**
+ * @param {Event} buttonClick
+ *
+ * @return {String} to print to HTML
+ */
+export function renderSettings() {
+  const mainContainer = document.querySelector("#mainCenter");
   mainContainer.innerHTML = `
   <div class="settings">
-  <div id="renderContainer" class="settings__container"></div>
-  <div class="settings__buttons">
-    <button
-      href="#"
-      id="plans"
-      onclick="renderPlans()"
-      class="btn settings__buttons-btn"
-    >
-      Plans
-    </button>
-    <button
-      href="#"
-      id="recipes"
-      onclick="renderRecipes()"
-      class="btn settings__buttons-btn"
-    >
-      Recipes
-    </button>
-    <button
-      href="#"
-      id="display"
-      onclick="renderButtonOptions()"
-      class="btn settings__buttons-btn"
-    >
-      Display
-    </button>
-    <button
-      href="#"
-      id="back"
-      onclick="reloadPage()"
-      class="btn settings__buttons-btn"
-    >
-      Back
-    </button>
+  <div class="settings__question">
+    <h1 class="settings__question-header">Select to change the button colour</h1>
+    <select name="buttons" id="btnColourSelect" class=""settings__question-button">
+       <option value="default">Default</option>
+       <option value="red">Red</option>
+       <option value="green">Green</option>
+       <option value="blue">Blue</option>
+    </select>
+    <button id="btnSelect" onclick="collectBtnSelect()" class="settings__question-btn-buttonSelect btn">Select</button>
+  </div>
+  <div class="settings__question">
+    <h1 class="settings__question-header">Press the button to change the text colour</h1>
+    <select name="buttons" id="textColourSelect" class=""settings__question-button">
+      <option value="default">Default</option>
+      <option value="red">Red</option>
+      <option value="green">Green</option>
+      <option value="blue">Blue</option>
+    </select>
+    <button id="btnSelect" onclick="collectTextSelect()" class="settings__question-btn-textSelect btn">Select</button>
+  </div>
+  <div class="settings__question">
+    <h1 class="settings__question-header">Press the button to change the background colour</h1>
+    <select name="buttons" id="backgroundColourSelect" class=""settings__question-button">
+      <option value="default">Default</option>
+      <option value="red">Red</option>
+      <option value="green">Green</option>
+      <option value="blue">Blue</option>
+    </select>
+    <button id="btnSelect" onclick="collectBackSelect()" class="settings__question-btn-backSelect btn">Select</button>
   </div>
   <button
     id="delete"
@@ -54,7 +111,7 @@ export function openSettings() {
   >
     Clear Storage
   </button>
-</div>
-  `;
+  </div>
+`;
   model.fetchSavedData();
 }
