2be3ede903d736c559e99b3d3517c25772583bf9
Joseph Woodland <joseph@workit-app.com>
2021-07-26 09:34:30 +0100

    Settings page redesign

--- assets/js/views/settings.js
+++ assets/js/views/settings.js
@@ -1,7 +1,7 @@
 import * as input from "./input.js";
 import * as model from "../model.js";
 
-const modalContainer = document.querySelector("#modal");
+const mainContainer = document.querySelector("#mainCenter");
 
 /**
  * @param {Event} buttonClick
@@ -10,71 +10,51 @@ const modalContainer = document.querySelector("#modal");
  */
 
 export function openSettings() {
-  input.openModal();
-  modalContainer.innerHTML = `
-  <div class="form__wrapper">
-        <div class="settings">
-          <div class="settings__head">
-            <img
-              class="settings__head-logo"
-              src="./assets/images/png/Logo.png"
-              alt="CookIt Logo"
-            />
-            <h1 class="settings__head-header">Settings</h1>
-            <a href="#" onclick="reloadPage()" class="settings__head-close"
-          ><i class="fas fa-times"></i
-        ></a>
-          </div>
-          <div id="renderContainer" class="settings__container">
-          </div>
-        </div>
-      </div>
-      <div class="settings__right">
-        <a href="#" onclick="reloadPage()" class="settings__right-close"
-          ><i class="fas fa-times"></i
-        ></a>
-        <div class="settings__right__buttons">
-        <div class="settings__right__buttons-left">
-          <button
-            href="#"
-            id="plans"
-            onclick="renderPlans()"
-            class="btn settings__right__buttons-btn"
-          >
-            Plans
-          </button>
-          <button
-            href="#"
-            id="recipes"
-            onclick="renderRecipes()"
-            class="btn settings__right__buttons-btn"
-          >
-            Recipes
-          </button>
-          </div>
-          <div class="settings__right__buttons-right">
-          <button
-            href="#"
-            id="display"
-            onclick="renderButtonOptions()"
-            class="btn settings__right__buttons-btn"
-          >
-            Display
-          </button>
-          <button
-            href="#"
-            id="back"
-            onclick="reloadPage()"
-            class="btn settings__right__buttons-btn"
-          >
-            Back
-          </button>
-          </div>
-          <button id="delete" onclick="clearStorage()" class="main__right-delete btn">
-            Clear Storage
-          </button>
-        </div>
-      </div>
+  mainContainer.innerHTML = `
+  <div class="settings">
+  <div id="renderContainer" class="settings__container"></div>
+  <div class="settings__buttons">
+    <button
+      href="#"
+      id="plans"
+      onclick="renderPlans()"
+      class="btn settings__buttons-btn"
+    >
+      Plans
+    </button>
+    <button
+      href="#"
+      id="recipes"
+      onclick="renderRecipes()"
+      class="btn settings__buttons-btn"
+    >
+      Recipes
+    </button>
+    <button
+      href="#"
+      id="display"
+      onclick="renderButtonOptions()"
+      class="btn settings__buttons-btn"
+    >
+      Display
+    </button>
+    <button
+      href="#"
+      id="back"
+      onclick="reloadPage()"
+      class="btn settings__buttons-btn"
+    >
+      Back
+    </button>
+  </div>
+  <button
+    id="delete"
+    onclick="clearStorage()"
+    class="settings__buttons-btn-clearStorage btn"
+  >
+    Clear Storage
+  </button>
+</div>
   `;
   model.fetchSavedData();
 }
